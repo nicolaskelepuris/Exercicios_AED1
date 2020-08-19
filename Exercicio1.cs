be19475b06322166fe3eafeb53456ec5039a8168
nicolas <nicolas.kelepuris@gmail.com>
2020-08-19 09:06:38 -0300

    fixed exercise 1

--- Exercicio1.cs
+++ Exercicio1.cs
@@ -14,9 +14,9 @@ namespace Exercicios_AED1
         float[] GetNumbers()
         {
             var numbersToSum = new float[2];
-            Console.WriteLine("Please inform first number to get the sum (using comma as decimal separator)");
-            numbersToSum[0] = float.Parse(Console.ReadLine());
-            Console.WriteLine("Please inform second number to get the sum (using comma as decimal separator)");
+            Console.WriteLine("Please inform first number to get the sum");
+            numbersToSum[0] = float.Parse(Console.ReadLine()); 
+            Console.WriteLine("Please inform second number to get the sum");
             numbersToSum[1] = float.Parse(Console.ReadLine());
             
             return numbersToSum;
