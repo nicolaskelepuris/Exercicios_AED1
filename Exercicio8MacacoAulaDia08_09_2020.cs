9088d87b2a2898d73edd3a24da51813782f1f761
nicolas <nicolas.kelepuris@gmail.com>
2020-09-08 21:29:13 -0300

    added exercises from class of the day 08_september_2020

---
+++ Exercicio8MacacoAulaDia08_09_2020.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios_AED1
+{
+    class Exercicio8MacacoAulaDia08_09_2020
+    {
+        public Exercicio8MacacoAulaDia08_09_2020(string nome)
+        {
+            Nome = nome;
+            ConteudoDoEstomago = estomagoVazio;
+        }
+
+        public string Nome { get; private set; }
+
+        private string estomagoVazio = "Estomago vazio";
+
+        private bool comeuComSucesso;
+
+        private bool digeriuComSucesso;
+
+        private string conteudoDoEstomago;
+
+        public string ConteudoDoEstomago
+        {
+            get { return conteudoDoEstomago; }
+            set
+            {
+                if (conteudoDoEstomago == estomagoVazio)
+                {
+                    // caso o macaco esteja de estomago vazio e mande digerir havera falha ao digerir
+                    if (value == estomagoVazio)
+                    {
+                        digeriuComSucesso = false;
+                    }
+
+                    // conteudo do estomago recebe o alimento caso o estomago esteja vazio
+                    conteudoDoEstomago = value;
+                    comeuComSucesso = true;
+                }
+                else
+                {
+                    if (value == estomagoVazio)
+                    {
+                        digeriuComSucesso = true;
+                    }
+
+                    comeuComSucesso = false;
+                    conteudoDoEstomago = value;
+                }
+            }
+        }
+
+        public void Comer(string alimento)
+        {
+            ConteudoDoEstomago = alimento;
+
+            if (comeuComSucesso)
+            {
+                Console.WriteLine("O macaco comeu com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("O macaco não conseguiu comer ja que ainda nao digeriu o alimento presente no estomago e jogou o novo alimento fora");
+            }
+        }
+
+        public void VerConteudoDoEstomago()
+        {
+            Console.WriteLine("Conteudo do Estomago: {0}", ConteudoDoEstomago);
+        }
+
+        public void DigerirConteudoDoEstomago()
+        {
+            ConteudoDoEstomago = estomagoVazio;
+
+            if(digeriuComSucesso)
+            {
+                Console.WriteLine("O macaco digeriu com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("O macaco não conseguiu digerir ja que seu estomago estava vazio");
+            }
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,12 +10,30 @@ namespace Exercicios_AED1
             //exercicio1.GetTwoNumbersAndPrintSum();
             //var exercicio2 = new Exercicio2();
             //exercicio2.GetFourGradesAndPrintAverage();
-            var exercicio01Aula04 = new Exercicio01Aula04();
-            exercicio01Aula04.PerformExercicio01Aula04();
-            var exercicio02Aula04 = new Exercicio02Aula04();
-            exercicio02Aula04.PerformExercicio02Aula04();
-            var exercicio03Aula04 = new Exercicio03Aula04();
-            exercicio03Aula04.PerformExercicio03Aula04();
+            //var exercicio01Aula04 = new Exercicio01Aula04();
+            //exercicio01Aula04.PerformExercicio01Aula04();
+            //var exercicio02Aula04 = new Exercicio02Aula04();
+            //exercicio02Aula04.PerformExercicio02Aula04();
+            //var exercicio03Aula04 = new Exercicio03Aula04();
+            //exercicio03Aula04.PerformExercicio03Aula04();
+            var macaco1 = new Exercicio8MacacoAulaDia08_09_2020("macaco1");
+            var macaco2 = new Exercicio8MacacoAulaDia08_09_2020("macaco2");
+
+            Console.WriteLine("Macaco 1:");
+            macaco1.VerConteudoDoEstomago();
+            macaco1.Comer("banana");
+            macaco1.VerConteudoDoEstomago();
+            macaco1.Comer("pimenta");
+            macaco1.DigerirConteudoDoEstomago();
+            macaco1.VerConteudoDoEstomago();
+
+            Console.WriteLine("\nMacaco 2:");
+            macaco2.DigerirConteudoDoEstomago();
+            macaco2.DigerirConteudoDoEstomago();
+            macaco2.Comer("piolho");
+            macaco2.VerConteudoDoEstomago();
+            macaco2.DigerirConteudoDoEstomago();
+            macaco2.VerConteudoDoEstomago();
         }
     }
 }
